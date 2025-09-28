0fbe94f23296ed9ea538781446f8c7c9c7af6885
Dainichi1 <torquati79@yahoo.it>
2025-09-28 19:48:28 +0200

    file eliminati/modificati - passaggio da java puro a springboot

--- src/main/java/unicam/filiera/controller/CriteriRicerca.java
+++
@@ -1,8 +0,0 @@
-package unicam.filiera.controller;
-
-public class CriteriRicerca {
-    public String testo;     // ricerca libera su nome/extra
-    public String stato;     // se valorizzato, filtra per stato
-    public String orderBy;   // "DATA" | "NOME" | "STATO"
-    public boolean asc = true;
-}
